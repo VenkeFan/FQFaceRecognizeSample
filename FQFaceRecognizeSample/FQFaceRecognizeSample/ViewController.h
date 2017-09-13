a129459fec9e32a7c743c789a613f59e7aec9002
qi.fan <fanqi_0817@live.com>
2017-09-13 11:16:34 +0800

    first

---
+++ FQFaceRecognizeSample/FQFaceRecognizeSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FQFaceRecognizeSample
+//
+//  Created by fanqi on 2017/9/13.
+//  Copyright © 2017年 fanqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
